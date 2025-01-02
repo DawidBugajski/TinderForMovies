08e435a08070e8bf5f95ad22babf97a0f5e02320
Dawid Bugajski <dawidbugajski1996@gmail.com>
2025-01-02 18:09:55 +0100

    feat: add ThemeProvider

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import Movies from '@/components/Movies';
 import StoreProvider from './StoreProvider';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <StoreProvider>
+      <Header />
       <Movies />
     </StoreProvider>
   );
